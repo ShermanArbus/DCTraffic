159fe00f62b6cc3bc318d12b505f662ae602f9ec
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-02 15:15:58 -0500

    split csv data on \n

--- src/parse.js
+++ src/parse.js
@@ -7,7 +7,7 @@ function parse() {
 let data = fileSystem.readFileSync("./moving_jan_2016.csv");
 
 let stringValue = data.toString();
-let cells = stringValue.split(",");
+let cells = stringValue.split("\n");
 console.log(cells);
 
 }
