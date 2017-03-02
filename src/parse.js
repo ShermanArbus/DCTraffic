450505368bf1235cc036e4380de2f8ad5f40b303
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-02 15:44:02 -0500

    parsed rows array into cell group arrays

--- src/parse.js
+++ src/parse.js
@@ -7,9 +7,15 @@ function parse() {
 let data = fileSystem.readFileSync("./moving_jan_2016.csv");
 
 let stringValue = data.toString();
-let cells = stringValue.split("\n");
-console.log(cells);
+let rows = stringValue.split("\n");
+// console.log(rows);
+let cellGroups = [];
 
-}
+for (i = 0; i < rows.length; i++) {
+  cellGroups.push(rows[i].split(","));
+};
+console.log(cellGroups);
+
+};
 
 parse();
