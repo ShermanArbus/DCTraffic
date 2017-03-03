3a2465f1bbdea1e0a625b584d687b5bd170b578a
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-03 15:14:55 -0500

    added functions to calculate total fines given, average fine

--- src/moving.js
+++ src/moving.js
@@ -3,29 +3,41 @@ let parsedData = require("./parse.js");
 function searchData() {
 
 let movingData = parsedData("./moving_jan_2016.csv");
-// console.log(movingData[0]);
 
-// console.log(movingData[0][9]);
 //add variable for each final value we're trying to find.
-//
-let finePaid = 0;
-let typeCount = 0;
-movingData.forEach(function findRow(row) {
-  //index 0 is title, last index is undefined  due to empty line at end of csv file.
-  // console.log(row[9]);
-  // console.log(row[10]);
-  // console.log(row[11]);
-  // console.log(row[17]);
-  let rowNum = new Number(row[11]);
-  console.log(rowNum);
-  if (row[9] === "Photo") {
 
-    finePaid = finePaid + rowNum;
-    typeCount++;
-    console.log(finePaid);
-    console.log(typeCount);
+let finePaid = 0; //total photo fines paid
+let photoCount = 0;
+let fineAmt = 0;
+let fineCount= 0;
+let fineAvg;
+
+movingData.forEach(function findRow(row) {
+  let row11Num = new Number(row[11]);
+  if (row[9] === "Photo") {
+    finePaid = finePaid + row11Num;
+    photoCount++;
+    // console.log(finePaid);
+    // console.log(photoCount);
   }
 });
+
+movingData.forEach(function findRow(row) {
+  let row10Num = new Number(row[10]);
+  if (row10Num >= 0) {
+    fineCount++;
+    fineAmt = fineAmt + row10Num;
+    fineAvg = fineAmt/fineCount;
+
+    console.log(fineCount);
+    console.log(row10Num);
+    console.log(fineAmt);
+    console.log(fineAvg);
+
+  }
+
+});
+
 }
 
 searchData();
