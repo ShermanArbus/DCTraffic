3eff57211339213dcc5260fd88bd359ae4315664
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-03 14:11:26 -0500

    converted fine paid values to numbers, added up total fines paid.

--- src/moving.js
+++ src/moving.js
@@ -6,12 +6,26 @@ let movingData = parsedData("./moving_jan_2016.csv");
 // console.log(movingData[0]);
 
 // console.log(movingData[0][9]);
+//add variable for each final value we're trying to find.
+//
+let finePaid = 0;
+let typeCount = 0;
+movingData.forEach(function findRow(row) {
+  //index 0 is title, last index is undefined  due to empty line at end of csv file.
+  // console.log(row[9]);
+  // console.log(row[10]);
+  // console.log(row[11]);
+  // console.log(row[17]);
+  let rowNum = new Number(row[11]);
+  console.log(rowNum);
+  if (row[9] === "Photo") {
 
-movingData.forEach(function functionName(row) {
-  console.log(row[9]);
-  console.log(row[10]);
-  console.log(row[11]);
-})
+    finePaid = finePaid + rowNum;
+    typeCount++;
+    console.log(finePaid);
+    console.log(typeCount);
+  }
+});
 }
 
 searchData();
