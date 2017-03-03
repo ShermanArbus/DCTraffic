4930cf1c1fc194bbc1dc47fdf31664a4b242c917
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-03 11:36:28 -0500

    can now output data for specific columns by spefiying desired index.

--- src/moving.js
+++ src/moving.js
@@ -1,10 +1,17 @@
 let parsedData = require("./parse.js");
 
-function stupidFunctionJerkface() {
+function searchData() {
 
 let movingData = parsedData("./moving_jan_2016.csv");
-console.log(movingData);
+// console.log(movingData[0]);
 
+// console.log(movingData[0][9]);
+
+movingData.forEach(function functionName(row) {
+  console.log(row[9]);
+  console.log(row[10]);
+  console.log(row[11]);
+})
 }
 
-stupidFunctionJerkface();
+searchData();
