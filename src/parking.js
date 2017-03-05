6aa1aa417f8c779d69041a64183d3d990ffe189a
Sara Basile <sarabasile@Saras-MacBook-Air.local>
2017-03-05 10:07:43 -0500

    added number of types of parking violations through object keys

--- src/parking.js
+++ src/parking.js
@@ -6,8 +6,13 @@ let parkingData = parsedData("./parking_feb_2016.csv");
 function codeFrequency() {
     let rowArray = [];
     parkingData.forEach(function findRow(row) {
-      // rowArray.push(row[9]); *****DON'T GET RID OF THIS *******
-      rowArray.push(row[12]); //<----state frequency*******
+      if (parkingData.indexOf(row) === 0 || parkingData.indexOf(row) === parkingData.length-1) {
+        return;
+      }
+      else {
+      rowArray.push(row[9]); //*****DON'T GET RID OF THIS *******
+      // rowArray.push(row[12]); //<----state frequency*******
+    }
     });
 
     let vCodeFreq = {};
@@ -19,6 +24,8 @@ function codeFrequency() {
         vCodeFreq[each] = 1;
       }
   });
+  let freqLength = Object.keys(vCodeFreq);
+  console.log(freqLength.length);
   console.log(vCodeFreq);
 }
 
